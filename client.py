0ff7e640dd3f4ddea3a60ce46e22937c58eadbc3
zsayem30 <sayemnazmuz@gmail.com>
2022-03-23 16:19:30 -0700

    Ros publisher added with Float64MultiArray

--- client.py
+++ client.py
@@ -24,7 +24,22 @@ def getMsg(prop):
 
 # def parseGazeData(data):
 
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
 
+if __name__ == '__main__':
+
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
 
 while True:
 	print('Sending command...')
@@ -45,6 +60,11 @@ while True:
 s.send(getMsg('ENABLE_SEND_DATA'))
 
 while True:
+
+	try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
 	try:
 		msg = s.recv(1024)
 		print(msg)
